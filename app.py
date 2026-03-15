32bc781939067568ee4f0d700fdf87a4c85b60e8
sanjaikumar2006 <sanjaibabu6385@gmail.com>
2026-03-15 20:06:39 +0530

    Update app.py
    edited last main area

--- app.py
+++ app.py
@@ -760,8 +760,9 @@ def student_profile(roll):
 
 
 # ================== MAIN ==================
+import os
+
 if __name__ == "__main__":
     init_db()
-    app.run(debug=True)
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
